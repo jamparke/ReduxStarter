f58297099566a1c83b43594e48ced20b5ff0b914
Andy Parker <andyparker84@gmail.com>
2017-11-01 11:50:43 -0500

    Project complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export function selectBook (book) {
-  // selectbook is an ActionCreator and needs to return an action, an object with a type property
+  // selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property.
   return {
     type: 'BOOK_SELECTED',
     payload: book
